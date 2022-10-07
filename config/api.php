633d59296a1964beadaf72657d14c33615250c7e
Dmitry P <dmitryp@martechstack.ai>
2022-10-07 10:49:18 -0700

    first commit

---
+++ config/api.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+      
+        'crdlink.site' => [
+        'name' => 'Credit Link',
+        'shortname'=> 'crdlink',
+        'domain' => 'crdlink.site',
+        'email' => 'contact@crdlink.site',
+        'location' => 'Port Charlotte, FL',
+        'phone' => '1 (800) 944-1838',
+        'address' => '1777 Tamiami Trail, Suite 402, Port Charlotte, FL 33948',
+      ],
+];
