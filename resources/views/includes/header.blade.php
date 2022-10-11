d380358b171b7ee9d8940508af41f55d941f6616
Dmitry P <dmitryp@martechstack.ai>
2022-10-11 12:44:10 -0700

    Menu fix

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -65,67 +65,25 @@
                 </div>
                 <nav class="mobile-menu">
                     <ul>
-                        <li>
-                            <a href="/">Demos</a>
-                            <ul class="sub-menu">
-                                <li><a href="/">Homepage Sass</a></li>
-                                <li><a href="index-2.html">Homepage SEO</a></li>
-                                <li><a href="index-3.html">Homepage App</a></li>
-                                <li><a href="index-4.html">Homepage Chatbot</a></li>
-                                <li><a href="index-5.html">Homepage Consultant</a></li>
-                                <li><a href="index-6.html">Homepage Fintech</a></li>
-                                <li><a href="index-7.html">Homepage Software</a></li>
-                                <li><a href="index-8.html">Homepage E-Wallet</a></li>
-                                <li><a href="index-9.html">Homepage Webinar</a></li>
-                                <li><a href="index-10.html">Homepage Fitness</a></li>
-                                <li><a href="index-11.html">Homepage Digital</a></li>
-                                <li><a href="index-12.html">Home Cryptocurrency</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="services.html">Services</a>
-                            <ul class="sub-menu">
-                                <li><a href="services.html">Our Services</a></li>
-                                <li><a href="service-details.html">Service Details</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="/#">Pages</a>
-                            <ul class="sub-menu">
-                                <li><a href="about.html">About Us</a></li>
-                                <li><a href="team.html">Our Team</a></li>
-                                <li><a href="pricing.html">Pricing</a></li>
-                                <li>
-                                    <a href="portfolio.html">Portfolio</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="portfolio.html">Portfolio One</a></li>
-                                        <li><a href="portfolio-2.html">Portfolio Two</a></li>
-                                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="shop.html">Shop</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-                                        <li><a href="shop-details.html">Shop Details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="sign-in.html">Sign In</a></li>
-                                <li><a href="sign-up.html">Sign Up</a></li>
-                                <li><a href="coming-soon.html">Coming Soon</a></li>
-                                <li><a href="404.html">404</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="blog-standard.html">Blog</a>
-                            <ul class="sub-menu">
-                                <li><a href="blog-standard.html">Blog Standard</a></li>
-                                <li><a href="blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="contact.html">Contact</a>
-                        </li>
+                            <li>
+                            <a href="millionaires-club">Millionaires Club</a>
+                            </li>
+                            <li>
+                                <a href="pricing">Pricing</a>
+                            </li>
+
+                            <li>
+                                <a href="about-us">About Us</a>
+                            </li>
+                            <li>
+                                <a href="products">Products</a>
+                            </li>
+                            <li>
+                                <a href="culture">Culture</a>
+                            </li>
+                            <li>
+                                <a href="contact-us">Contact</a>
+                            </li>
                     </ul>
                 </nav>
                 <a href="/#" class="panel-close">
